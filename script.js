54b72390bd1c248f3638bf80fd72cca0bfce1e34
Sportsguy <mahoward2s@gmail.com>
2020-11-24 19:25:44 -0600

    adding some functions, and randomness; and major bugs

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 //Variables//
 var startQuiz = document.querySelector (".startButton")
 var instructions = document.querySelector (".Instructions")
-
-
-//Start Quiz Button//
+var secondsLeft = 240;
+var highScore = 
 
 
 //Form//
 //Questions//
 //Timer//
+function setTime() {
+    var timerInterval = setInterval(function (){
+        secondsLeft--;
+        timerInterval.textContent = secondsLeft + "Seconds Left In Quiz";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+
 //Local Storage//
 //high score//
+
+//Start Quiz Button//
+startQuiz.addEventListener('click', function(){
+    
+    
+})
